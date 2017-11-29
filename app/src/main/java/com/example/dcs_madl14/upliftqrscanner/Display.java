59ba07bae359f1e3eea327aa0ac41f693f8c3291
jmrequioma <jerome.requioma@gmail.com>
2017-11-29 15:27:06 +0800

    wrong

--- app/src/main/java/com/example/dcs_madl14/upliftqrscanner/Display.java
+++ app/src/main/java/com/example/dcs_madl14/upliftqrscanner/Display.java
@@ -41,7 +41,7 @@ public class Display extends Activity {
     TextView destination_label;
     TextView bus_label;
     TextView loading_label;
-    TextView weight_label;
+    //TextView weight_label;
     TextView fee_label;
     TextView status_value;
     TextView passenger_value;
@@ -49,7 +49,7 @@ public class Display extends Activity {
     TextView destination_value;
     TextView bus_value;
     TextView loading_value;
-    TextView weight_value;
+    //TextView weight_value;
     TextView fee_value;
 
     ImageView check;
@@ -72,14 +72,14 @@ public class Display extends Activity {
         destination_label = (TextView) findViewById(R.id.destination_label);
         bus_label = (TextView) findViewById(R.id.bus_label);
         loading_label = (TextView) findViewById(R.id.loading_label);
-        weight_label = (TextView) findViewById(R.id.weight_label);
+        //weight_label = (TextView) findViewById(R.id.weight_label);
         fee_label = (TextView) findViewById(R.id.fee_label);
         passenger_value = (TextView) findViewById(R.id.passenger_value);
         via_value = (TextView) findViewById(R.id.via_value);
         destination_value = (TextView) findViewById(R.id.destination_value);
         bus_value = (TextView) findViewById(R.id.bus_value);
         loading_value = (TextView) findViewById(R.id.loading_value);
-        weight_value = (TextView) findViewById(R.id.weight_value);
+        //weight_value = (TextView) findViewById(R.id.weight_value);
         fee_value = (TextView) findViewById(R.id.fee_value);
         btnScan = (Button) findViewById(R.id.btnScan);
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -102,14 +102,14 @@ public class Display extends Activity {
         destination_label.setTypeface(montserratReg);
         bus_label.setTypeface(montserratReg);
         loading_label.setTypeface(montserratReg);
-        weight_label.setTypeface(montserratReg);
+        //weight_label.setTypeface(montserratReg);
         fee_label.setTypeface(montserratReg);
         passenger_value.setTypeface(montserratBold);
         via_value.setTypeface(montserratBold);
         destination_value.setTypeface(montserratBold);
         bus_value.setTypeface(montserratBold);
         loading_value.setTypeface(montserratBold);
-        weight_value.setTypeface(montserratBold);
+        //weight_value.setTypeface(montserratBold);
         fee_value.setTypeface(montserratBold);
         btnScan.setTypeface(montserratReg);
         Intent i = getIntent();
@@ -182,12 +182,12 @@ public class Display extends Activity {
                 String passenger = displayName(response);
                 String fee = CURR + " " + displayFee(response);
                 String lBay = displayLoadingBay(response);
-                String weight = displayWeight(response) + " " + WEIGHT_UNIT;
+                //String weight = displayWeight(response) + " " + WEIGHT_UNIT;
                 id = lBay;
                 passenger_value.setText(passenger);
                 fee_value.setText(fee);
                 //loading_value.setText(lBay);
-                weight_value.setText(weight);
+                //weight_value.setText(weight);
                 dialog.dismiss();
             }
             //progressBar.setVisibility(View.GONE);
